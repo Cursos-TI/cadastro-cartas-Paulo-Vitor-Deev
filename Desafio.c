14d53bef59e9d637fe62dc91cff6b32a04294804
Paulo-Vitor-Deev <paulovitor8845@outlook.com>
2026-02-13 08:54:02 -0300

    Desafio nível 2
    Calcular densidade populacional e o pib per capita.

--- Desafio.c
+++ Desafio.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-// Desafio super trunfo - Países
-// cadastro carta1 - Variáveis
-int main(){
-// Variáveis
-    char estado1, estado2;
-    char codigo1[5], códigos[5];
-    char cidade1[20], cidade2[20];
-    int populaçao1, população 2;
-    float areakm1,areakm2;
-    float pib1,pib2;
-    int pontosturisticos1, pontosturisticos2;
 
-// Dados variáveis 1    
+int main(){
+    #include <stdio.h>
+// Desafio super trunfo - Países
+// cadastro carta1 - Variaveis
+    char estado1, estado2, codigo1[5], codigo2[5], cidade1[20], cidade2[20]; 
+    int populacao1, populacao2, pontosturisticos1, pontosturisticos2;
+    float areakm1, areakm2, pib1, pib2, densidadepopulacional1, densidadepopulacional2, pibpercapita1, pibpercapita2;
+
+// Dados carta 1
     printf("Digite a letra correspondente ao seu estado:\n");
     scanf("%c", &estado1);
 
@@ -22,7 +19,7 @@ int main(){
     scanf("%s", &cidade1);
 
     printf("Digite a quantidade de habitantes de sua cidade:\n");
-    scanf("%d", &populaçao1);
+    scanf("%d", &populacao1);
 
     printf("Digite a área em km da sua cidade:\n");
     scanf("%f", &areakm1);
@@ -31,9 +28,12 @@ int main(){
     scanf("%f", &pib1);
 
     printf("Digite a quantos pontos turisticos existem em sua cidade:\n");
-    scanf("%d", &pontosturisticos1);  
+    scanf("%d", &pontosturisticos1);
 
-// Dados variaveis 2
+    densidadepopulacional1 = (float) populacao1 / areakm1;
+    pibpercapita1 = (float) pib1 / populacao1;
+
+// Dados carta 2
     printf("\nDigite a letra correspondente ao seu estado:\n");
     scanf("%c", &estado2);
 
@@ -44,7 +44,7 @@ int main(){
     scanf("%s", &cidade2);
 
     printf("Digite a quantidade de habitantes de sua cidade:\n");
-    scanf("%d", &populaçao2);
+    scanf("%d", &populacao2);
 
     printf("Digite a área em km da sua cidade:\n");
     scanf("%f", &areakm2);
@@ -54,21 +54,27 @@ int main(){
 
     printf("Digite a quantos pontos turisticos existem em sua cidade:\n");
     scanf("%d", &pontosturisticos2);
+// calculos
+    densidadepopulacional2 = (float) populacao2 / areakm2;
+    pibpercapita2 = (float) pib2 / populacao2;
 
-// Dados cartas 1 
-    printf("Estado: %c - Codigo: %s\n", estado1, codigo1);
-    printf("Cidade: %s - População: %d\n", cidade1, populaçao1);
-    printf("Área em km: %f - Pib: %f\n", areakm1, pib1);
+// Resultado 1 - carta
+    printf("\nEstado: %c - Codigo: %s\n", estado1, codigo1);
+    printf("Cidade: %s - População: %d\n", cidade1, populacao1);
+    printf("Área em km: %f - Pib: %f\n", pib1);
     printf("Pontos turisticos: %d\n", pontosturisticos1);
+    printf("A densidade populacional da cidade é: %.2f\n", &densidadepopulacional1);
+    printf("O pib per capita é: %.2f\n", pibpercapita1);
 
-// Dados cartas 2
+// Resultado 2 - carta
     printf("\nEstado: %c - Codigo: %s\n", estado2, codigo2);
-    printf("Cidade: %s - População: %d\n", cidade2, populaçao2);
-    printf("Área em km: %f - Pib: %f\n", areakm2, pib2);
+    printf("Cidade: %s - População: %d\n", cidade2, populacao2);
+    printf("Área em km: %f - Pib: %f\n", pib2);
     printf("Pontos turisticos: %d\n", pontosturisticos2);
+    printf("A densidade populacional da cidade é: %.2f\n", &densidadepopulacional2);
+    printf("O pib per capita é: %.2f\n", pibpercapita2);
 
     return 0;
 
 
 }
-
